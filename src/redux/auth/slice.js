427a0fff216a7d8c4d26039931cb810cbbee59f2
Kyryl2 <krasiuk.4884@gmail.com>
2024-05-21 14:28:38 +0200

    start ok

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase();
+  },
+});
+
+export const authReducer = authSlice.reducer;
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,16 +1,13 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { selectNameFilter } from "../filters/selectors";
 
-export const selectContacts = (state) => state.contacts.items;
+export const selectContacts = (state) => state.contacts.contacts.items;
 export const getIsLoading = (state) => state.contacts.loading;
 
 export const getError = (state) => state.contacts.error;
 const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
-  (items, { name }) => {
-    items.filter((item) =>
-      item.name.toLowerCase().includes(name.toLowerCase())
-    );
-  }
+  (items, name) =>
+    items.filter((item) => item.name.toLowerCase().includes(name.toLowerCase()))
 );
 export default selectFilteredContacts;
